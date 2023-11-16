00437020e4ce82a357e52ba4cb7cfadc4da71aab
vinsunkavalli <vinsunkavalli@gmail.com>
2023-11-16 00:22:07 -0600

    modified model due to torch constraints

--- model.py
+++ model.py
@@ -16,37 +16,38 @@ import torch.nn.functional as F
 
 from collections import OrderedDict
 
-class NeuralNetwork(nn.Module):
-    def __init__(self, num_indexes, hidden_dim, num_hidden):
+
+class CostNetwork(nn.Module):
+    def __init__(self, num_indexes, num_hidden, hidden_dim):
         assert num_indexes > 0
         assert hidden_dim > 0
         assert num_hidden > 0
 
-        super(NeuralNetwork, self).__init__()
+        super(CostNetwork, self).__init__()
 
         self.num_indexes = num_indexes #input and output dim
-        self.hidden_dim = hidden_dim
         self.num_hidden = num_hidden
+        self.hidden_dim = hidden_dim
 
-        self.flatten = nn.Flatten()
+        #self.flatten = nn.Flatten()
 
         layers = OrderedDict()
     
-        layers['fc1'] = nn.Linear(num_indexes, hidden_dim)
-        layers['relu1'] == nn.ReLU()
+        layers['fc1'] = nn.Linear(in_features=num_indexes, out_features=hidden_dim)
+        layers['relu1'] = nn.ReLU()
 
         for i in range(2, num_hidden):
             layers['fc' + str(i)] = nn.Linear(hidden_dim, hidden_dim)
             layers['relu' + str(i)] = nn.ReLU()
 
-        layers['out'] = nn.Linear(hidden_dim, num_indexes)
+        layers['out'] = nn.Linear(hidden_dim, 1)
 
         self.neural_net = nn.Sequential(layers)
 
     def forward(self, x):
         # input: x is the prior state, shape is (1, num_indexes)
         # output: y is the recommended state, shape is (num_indexes) 
-        flatten = self.flatten(x)
-        out = self.neural_net(flatten)
+        #flatten = torch.flatten(x, 1)
+        out = self.neural_net(x)
 
         return out
